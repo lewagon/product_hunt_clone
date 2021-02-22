887990526d494390a5c4e27441a091c2c7f76185
Andy B <andybarnov@gmail.com>
2021-02-22 14:19:08 +0100

    refine system test case to assert Product count too

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url
-
     assert_selector "h1", text: "Awesome Products"
+    assert_selector ".card-product", count: Product.count
   end
 end
